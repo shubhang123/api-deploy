79f0a6f08d22b92d0eb710a2bdea0032c0cc7e00
shubhang <shubhang.chakrawarty@gmail.com>
2025-05-11 11:50:36 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from typing import List, Union
+
+app = FastAPI()
+
+
+# --- Request schema ---
+class InputData(BaseModel):
+    data: List[Union[str, int]]
+
+
+# --- Response route ---
+@app.post("/bfhl")
+async def process_data(input: InputData):
+    user_id = "sdasdsad"  
+    email = "ajsdnsd@xyz.com"                
+    roll_number = "ABCD123"
+
+    numbers = []
+    alphabets = []
+
+    for item in input.data:
+        item_str = str(item)
+        if item_str.isnumeric():
+            numbers.append(item_str)
+        elif item_str.isalpha():
+            alphabets.append(item_str)
+
+    response = {
+        "is_success": True,
+        "user_id": user_id,
+        "email": email,
+        "roll_number": roll_number,
+        "numbers": numbers,
+        "alphabets": alphabets
+    }
+
+    return response
